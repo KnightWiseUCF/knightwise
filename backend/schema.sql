105f1e6f8ea619c7e3b3f5a1f5d9fd9eadd8da02
Daniel Landsman <da606808@ucf.edu>
2026-02-09 22:03:28 -0500

    SCRUM-146: New MySQL service

---
+++ backend/schema.sql
@@ -0,0 +1,163 @@
+----------------------------------------------------------------
+--
+--  Project:       KnightWise
+--  Year:          2026
+--  Author(s):     Daniel Landsman
+--  File:          schema.sql
+--  Description:   KnightWise database schema
+--                 Should be kept in sync with KW_Testing
+--
+----------------------------------------------------------------
+
+-- MySQL dump 10.13  Distrib 8.4.7, for Linux (x86_64)
+--
+-- Host: localhost    Database: KnightWise
+-- ------------------------------------------------------
+-- Server version       8.4.7-0ubuntu0.25.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `AnswerText`
+--
+
+DROP TABLE IF EXISTS `AnswerText`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `AnswerText` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `QUESTION_ID` int DEFAULT NULL,
+  `IS_CORRECT_ANSWER` tinyint(1) DEFAULT NULL,
+  `TEXT` text,
+  `RANK` int DEFAULT NULL,
+  `PLACEMENT` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `QUESTION_ID` (`QUESTION_ID`),
+  CONSTRAINT `AnswerText_ibfk_1` FOREIGN KEY (`QUESTION_ID`) REFERENCES `Question` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=279 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `EmailCode`
+--
+
+DROP TABLE IF EXISTS `EmailCode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `EmailCode` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `EMAIL` varchar(255) NOT NULL,
+  `OTP` varchar(6) NOT NULL,
+  `EXPIRES` datetime NOT NULL,
+  `IS_VERIFIED` tinyint(1) DEFAULT '0',
+  `CREATED_AT` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `EMAIL` (`EMAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Professor`
+--
+
+DROP TABLE IF EXISTS `Professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Professor` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `PASSWORD` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `FIRSTNAME` varchar(100) DEFAULT NULL,
+  `LASTNAME` varchar(100) DEFAULT NULL,
+  `USERNAME` varchar(255) DEFAULT NULL,
+  `VERIFIED` smallint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Question`
+--
+
+DROP TABLE IF EXISTS `Question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Question` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `TYPE` varchar(50) DEFAULT NULL,
+  `AUTHOR_EXAM_ID` varchar(100) DEFAULT NULL,
+  `SECTION` varchar(100) DEFAULT NULL,
+  `CATEGORY` varchar(100) DEFAULT NULL,
+  `SUBCATEGORY` varchar(100) DEFAULT NULL,
+  `POINTS_POSSIBLE` decimal(5,2) DEFAULT NULL,
+  `QUESTION_TEXT` text,
+  `OWNER_ID` int DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_Question_Owner` (`OWNER_ID`),
+  CONSTRAINT `FK_Question_Owner` FOREIGN KEY (`OWNER_ID`) REFERENCES `User` (`ID`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Response`
+--
+
+DROP TABLE IF EXISTS `Response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Response` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `USERID` int DEFAULT NULL,
+  `PROBLEM_ID` int DEFAULT NULL,
+  `DATETIME` datetime DEFAULT CURRENT_TIMESTAMP,
+  `ISCORRECT` tinyint(1) DEFAULT NULL,
+  `CATEGORY` varchar(100) DEFAULT NULL,
+  `TOPIC` varchar(100) DEFAULT NULL,
+  `POINTS_EARNED` decimal(5,2) DEFAULT NULL,
+  `POINTS_POSSIBLE` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `USERID` (`USERID`),
+  KEY `PROBLEM_ID` (`PROBLEM_ID`),
+  CONSTRAINT `Response_ibfk_1` FOREIGN KEY (`USERID`) REFERENCES `User` (`ID`) ON DELETE CASCADE,
+  CONSTRAINT `Response_ibfk_2` FOREIGN KEY (`PROBLEM_ID`) REFERENCES `Question` (`ID`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `PASSWORD` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `FIRSTNAME` varchar(100) DEFAULT NULL,
+  `LASTNAME` varchar(100) DEFAULT NULL,
+  `USERNAME` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `EMAIL` (`EMAIL`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-10  2:50:36
