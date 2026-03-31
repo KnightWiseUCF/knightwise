91770cf3272e2dfbe092e9b1c70a569224741610
will f. <will@logicmountain.com>
2026-03-30 20:35:35 -0400

    Refactor imports in App component and clean up routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,9 +28,9 @@ import AccountPage from "./pages/AccountPage";
 import ProfessorApplyPage from "./pages/ProfessorApplyPage";
 import ProfessorDraftsPage from "./pages/ProfessorDraftsPage";
 import ProfilePage from "./pages/ProfilePage";
-import StorePage from "./pages/StorePage.tsx";
-import LeaderboardPage from "./pages/LeaderboardPage.tsx";
-import ProfessorStatisticsPage from "./pages/ProfessorStatisticsPage.tsx";
+import StorePage from "./pages/StorePage";
+import LeaderboardPage from "./pages/LeaderboardPage";
+import ProfessorStatisticsPage from "./pages/ProfessorStatisticsPage";
 
 
 function App() 
@@ -57,9 +57,7 @@ function App()
         <Route path="/leaderboard"                element={<LeaderboardPage />} />
         <Route path="/guilds"                     element={<GuildsPage />} />
         <Route path="/guilds/:id"                 element={<GuildsPage />} />
-        {/* <Route path="/professor-statistics"        element={<ProfessorStatisticsPage />} /> */}
         <Route path="/professor-stats"            element={<ProfessorStatisticsPage />} />
-        <Route path="/guilds"                     element={<GuildsPage />} />
       </Routes>
     </Router>
   );
